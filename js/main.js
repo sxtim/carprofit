8ff0c4a3c5b71490279036cceff2a1bf39e658a9
sxtim <sxtimx@gmail.com>
2023-10-16 01:39:04 +0300

    add adaptive section docs

--- js/main.js
+++ js/main.js
@@ -1,42 +1,58 @@
 // CAROUSEL
-$(function(){
-  $('.carousel__inner').slick({
-    arrows: false,
-    dots: true,
-    slidesToShow: 3,
-  });
-  //
-  // $('.carousel__inner').slick({
-  //   arrows: false,
-  //   dots: true,
-  //   slidesToShow: 3,
-  //   responsive: [
-  //     {
-  //       breakpoint: 841,
-  //       settings: {
-  //         slidesToShow: 2
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 601,
-  //       settings: {
-  //         slidesToShow: 1
-  //       }
-  //     },
-  //   ]
-  // });
-  //
-  // wow = new WOW(
-  //   {
-  //     boxClass: 'wow',      // default
-  //     animateClass: 'animate__animated', // default
-  //     offset: 0,          // default
-  //     mobile: true,       // default
-  //     live: true        // default
-  //   }
-  // )
-  // wow.init();
-  //
+$(function () {
+    $('.carousel__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 2,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 540,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+    //
+    // $('.carousel__inner').slick({
+    //   arrows: false,
+    //   dots: true,
+    //   slidesToShow: 3,
+    //   responsive: [
+    //     {
+    //       breakpoint: 841,
+    //       settings: {
+    //         slidesToShow: 2
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 601,
+    //       settings: {
+    //         slidesToShow: 1
+    //       }
+    //     },
+    //   ]
+    // });
+    //
+    // wow = new WOW(
+    //   {
+    //     boxClass: 'wow',      // default
+    //     animateClass: 'animate__animated', // default
+    //     offset: 0,          // default
+    //     mobile: true,       // default
+    //     live: true        // default
+    //   }
+    // )
+    // wow.init();
+    //
 });
 
 // BURGER
@@ -44,5 +60,5 @@ const menuBtn = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu__list');
 
 menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('menu__list--active');
+    menu.classList.toggle('menu__list--active');
 })
