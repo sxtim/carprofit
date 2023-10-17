040604a30d7e9c6d4c770d358d15243c34efe30d
sxtim <sxtimx@gmail.com>
2023-10-17 11:38:40 +0300

    fixed bug imgages

--- js/main.js
+++ js/main.js
@@ -22,26 +22,6 @@ $(function () {
         ]
     });
     //
-    // $('.carousel__inner').slick({
-    //   arrows: false,
-    //   dots: true,
-    //   slidesToShow: 3,
-    //   responsive: [
-    //     {
-    //       breakpoint: 841,
-    //       settings: {
-    //         slidesToShow: 2
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 601,
-    //       settings: {
-    //         slidesToShow: 1
-    //       }
-    //     },
-    //   ]
-    // });
-    //
     // wow = new WOW(
     //   {
     //     boxClass: 'wow',      // default
