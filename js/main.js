bacea68cc0c172f362daeb00804d76b9f65b9c40
sxtim <sxtimx@gmail.com>
2023-10-29 19:32:19 +0300

    refactor structure html

--- js/main.js
+++ js/main.js
@@ -21,18 +21,6 @@ $(function () {
             }
         ]
     });
-    //
-    // wow = new WOW(
-    //   {
-    //     boxClass: 'wow',      // default
-    //     animateClass: 'animate__animated', // default
-    //     offset: 0,          // default
-    //     mobile: true,       // default
-    //     live: true        // default
-    //   }
-    // )
-    // wow.init();
-    //
 });
 
 //***BURGER***
